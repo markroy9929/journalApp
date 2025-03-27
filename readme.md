a7262c89285a7968d89e2145a2458ec6e229a635
Ankit Tayal <tayalankit20@gmail.com>
2025-03-27 17:18:33 +0530

    additional commit March 2025

--- readme.md
+++ readme.md
@@ -1,8 +1,19 @@
-Hello
+Hello welcome to my experiment lab
 
+
+Completed till Lecture/Video:  # 8
+  
+run daily:
+```
+c:; cd 'c:\Users\ANKIT\Documents\Intellij\journalApp'; git add -A; git commit -m "additional commit March 2025"; git push;
+```
+
+### [Spring Boot Mastery: From Basics to Advanced](https://www.youtube.com/playlist?list=PLA3GkZPtsafacdBLdd3p1DyRd5FGfr3Ue)
+<!-- 
 git init
 git add .
-git commit -m "first commit"
+git commit -m "march 2025"
 git branch -M main
 git remote add origin https://github.com/ankitT20/journalApp.git
 git push -u origin main
+ -->
--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -1,15 +1,18 @@
-package net.engineeringdigest.journalApp;
+package net.engineeringdigest.journalApp;  // es pakage ki sari @Component wali classes IOC container rakh lega
+// koi aur pakage (parent folder) ko IOC scan nahi krega
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication   // merge of these 3 anotations - @Configuration @EnableAutoConfiguration  @ComponentScan
 public class JournalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
-
 
 }
