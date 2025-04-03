ff97843af18d466e01187155adfcc3c500a93ec1
Ankit Tayal <tayalankit20@gmail.com>
2025-04-03 17:21:49 +0530

    add auth April 2025

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -30,7 +30,7 @@ public class JournalEntryService {
             journalentry.setDate(LocalDateTime.now());
             JournalEntry saved = journalEntryRepository.save(journalentry);
             user.getJournalEntries().add(saved);
-            userService.saveEntry(user);
+            userService.saveUser(user);
         } catch (Exception e) {
             System.out.println(e);
             throw new RuntimeException("An error occurred while saving the entry",e);
@@ -49,12 +49,25 @@ public class JournalEntryService {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id, String userName) {
-        User user = userService.findByUserName(userName);
-        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
-        userService.saveEntry(user);
-        journalEntryRepository.deleteById(id);
+    @Transactional
+    public boolean deleteById(ObjectId id, String userName) {
+        boolean removed = false;
+        try {
+            User user = userService.findByUserName(userName);
+            removed = user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+            if (removed) {
+                userService.saveUser(user);
+                journalEntryRepository.deleteById(id);
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while deleting the entry",e);
+        }
+        return removed;
+
     }
+
+//    public List<JournalEntry> findByUserName(String userName) {}
 
 }
 
--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -20,13 +20,13 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveNewEntry(User user) {
+    public void saveNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
     }
 
-    public void saveEntry(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
+    public void saveUser(User user) {
         userRepository.save(user);
     }
 
