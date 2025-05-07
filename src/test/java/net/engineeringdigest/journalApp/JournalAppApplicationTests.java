8dbe1a48729f4be2cfc94316afb69f48c8947a2a
Ankit Tayal <tayalankit20@gmail.com>
2025-05-07 17:56:44 +0530

    additional commit 7 May 2025

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 //@ActiveProfiles("dev")
-//@SpringBootTest
+@SpringBootTest
 class JournalAppApplicationTests {
 
-	@Disabled
+//	@Disabled
 	@Test
 	void contextLoads() {
 	}
